32f05392835a50c433ff6047036102af22918013
Marat Kashaev <kashaevmt@gmail.com>
2018-05-04 15:09:34 +0400

    new fsm

--- src/lab4.py
+++ src/lab4.py
@@ -6,24 +6,24 @@ def main():
 	fsm = FSMRule()
 
 	fsm.add_alphabet(('a', 'b', 'c', 'd'))
-	fsm.add_states(('q0', 'q1', 'q2', 'q3', 'F'))
+	fsm.add_states(('q0', 'q1', 'q2', 'q3'))
 
 	fsm.set_initial_state('q0')
 
-	fsm.add_final_states(('q0', 'q2', 'F'))
+	fsm.add_final_states(('q0', 'q1', 'q3'))
 
 	print(f"All states: {fsm.states}")
 	print(f"Current state: {fsm.current_state}")
 	print(f"Is it finish state? {fsm.is_finish()}")
 	print("======================================")
 
-	fsm.add_transitions(('q0', 'a', 'q1'))
-	fsm.add_transitions(('q1', 'b', 'F'))
-	fsm.add_transitions(('q0', 'c', 'F'))
-	fsm.add_transitions(('q0', 'd', 'q2'))
-	fsm.add_transitions(('q2', 'd', 'q2'))
+	fsm.add_transitions(('q0', 'a', 'q2'))
+	fsm.add_transitions(('q2', 'b', 'q1'))
+	fsm.add_transitions(('q0', 'c', 'q1'))
+	fsm.add_transitions(('q0', 'd', 'q3'))
+	fsm.add_transitions(('q3', 'd', 'q3'))
 
-	word = "cd"
+	word = input("Enter word: ")
 
 	for i in word:
 		fsm.trans(i)
